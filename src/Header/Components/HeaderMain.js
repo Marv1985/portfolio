852c60b53dfab8148e773fc0a1be9817016f48a8
Marv1985 <marvinshannon@hotmail.co.uk>
2023-03-04 17:05:30 +0000

    header section with slide out menu done but has no links functionaility

--- src/Header/Components/HeaderMain.js
+++ src/Header/Components/HeaderMain.js
@@ -1,12 +1,12 @@
 import HeaderIntro from "./HeaderIntro";
-import HomeButton from "./HomeButton";
+import MenuButton from "./MenuButton";
 import ParticlesConfig from "./ParticlesConfig";
 import "/home/marv/react-projects/portfolio/src/Header/Css/headerMain.css";
 
 export default function HeaderMain() {
   return (
     <div>
-      <HomeButton />
+      <MenuButton />
       <ParticlesConfig />
       <HeaderIntro />
     </div>
---
+++ src/Header/Components/MenuButton.js
@@ -0,0 +1,35 @@
+import deathStar from "/home/marv/react-projects/portfolio/src/Header/images/death-star.svg";
+import "/home/marv/react-projects/portfolio/src/Header/Css/homeButton.css";
+import "/home/marv/react-projects/portfolio/src/Header/Css/menuButton.css";
+import { useState, useCallback } from "react";
+
+export default function MenuButton() {
+  const [show, setShow] = useState(true);
+
+  const handleToggle = useCallback(() => setShow((prevShow) => !prevShow), []);
+
+  return (
+    <div>
+      {/* menu popout */}
+      <div>
+        <div className={show ? "menu-close" : "menu-overlay"}></div>
+        <div className={show ? "menu-close2" : "menu-links"}>
+          <span onClick={handleToggle}>
+            <i className="arrow left"></i>
+          </span>
+          <ul className="options">
+            <li>Home</li>
+            <li>About</li>
+            <li>My Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* menu button */}
+      <div className="home-button">
+        <img onClick={handleToggle} src={deathStar} alt="death star" />
+      </div>
+    </div>
+  );
+}
